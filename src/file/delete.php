500a47e6111d7a32c4edd0acee467bd02c729dab
programarivm <info@programarivm.com>
2019-08-15 09:13:51 +0100

    security fix

--- src/file/delete.php
+++ src/file/delete.php
@@ -11,13 +11,13 @@ $row = $result->fetch_array(MYSQLI_ASSOC);
 
 $filepath = APP_PATH."/storage/{$row['uniqid']}";
 
-if (empty($row)) {
-    http_response_code(404);
-    $body = ['message' => 'Not Found'];
-    print_r(json_encode($body, true));
-} elseif (!filter_var($id, FILTER_VALIDATE_INT)) {
+if (!filter_var($id, FILTER_VALIDATE_INT)) {
     http_response_code(400);
     $body = ['message' => 'Bad Request'];
+    print_r(json_encode($body, true));
+} elseif (empty($row)) {
+    http_response_code(404);
+    $body = ['message' => 'Not Found'];
     print_r(json_encode($body, true));
 } elseif (file_exists($filepath)) {
     $sql = "DELETE FROM files WHERE id='$id'";

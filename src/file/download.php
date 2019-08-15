c28b3534674899d8146d35e4ec9e69fe10de18c5
programarivm <info@programarivm.com>
2019-08-15 09:17:45 +0100

    security fix

--- src/file/download.php
+++ src/file/download.php
@@ -4,17 +4,20 @@ use MyFiles\Utils\DB;
 
 $id = DB::getInstance()->escape(explode('/', $_SERVER['REQUEST_URI'])[3]);
 
+if (!filter_var($id, FILTER_VALIDATE_INT)) {
+    http_response_code(400);
+    $body = ['message' => 'Bad Request'];
+    print_r(json_encode($body, true));
+    exit;
+}
+
 $sql = "SELECT * FROM files WHERE id='$id'";
 $result = DB::getInstance()->query($sql);
 $row = $result->fetch_array(MYSQLI_ASSOC);
 
 $filepath = APP_PATH."/storage/{$row['uniqid']}/{$row['name']}";
 
-if (!filter_var($id, FILTER_VALIDATE_INT)) {
-    http_response_code(400);
-    $body = ['message' => 'Bad Request'];
-    print_r(json_encode($body, true));
-} elseif (empty($row)) {
+if (empty($row)) {
     http_response_code(404);
     $body = ['message' => 'Not Found'];
     print_r(json_encode($body, true));
